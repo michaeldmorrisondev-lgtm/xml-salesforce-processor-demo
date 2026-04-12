ca94c0634a29a3d003d983e9e534f96e5facd141
Mike Morrison <michaeldmorrison121@gmail.com>
2026-04-12 16:02:33 -0400

    initial setup

---
+++ xmlproject/src/main/java/com/mmp/xmlproject/XmlprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mmp.xmlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlprojectApplication.class, args);
+	}
+
+}
---
+++ xmlproject/src/test/java/com/mmp/xmlproject/XmlprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmp.xmlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
