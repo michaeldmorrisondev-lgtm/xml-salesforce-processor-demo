e80079dae9bbde06a5f0de92cd46dba4fa45f439
michaeldmorrisondev-lgtm <michael.d.morrison@gmail.com>
2026-04-28 22:23:55 -0400

    added EnableAsync annotation to main class

--- xmlproject/src/main/java/com/mmp/xmlproject/XmlprojectApplication.java
+++ xmlproject/src/main/java/com/mmp/xmlproject/XmlprojectApplication.java
@@ -2,8 +2,10 @@ package com.mmp.xmlproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class XmlprojectApplication {
 
 	public static void main(String[] args) {
